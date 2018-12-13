b3669a0f0dd95d98822cc08bc86280d89e7de700
Michael Devery <michaeldevery@gmail.com>
2018-12-13 15:07:03 +0000

    Remove sample data on install

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 docker-compose up
-docker-compose exec -u buildkit civicrm civibuild create nasc --type drupal-clean --civi-ver 5.7.2 --url http://localhost:8080
+docker-compose exec -u buildkit civicrm civibuild create nasc --type drupal-clean --no-sample-data --civi-ver 5.7.2 --url http://localhost:8080
